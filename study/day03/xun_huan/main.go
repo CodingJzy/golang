15ae66ec643f3c6a60e1991a0c84741f1d46e02b
jiang_wei <jiang_wei@topsec.com.cn>
2019-06-26 15:51:31 +0800

    add for循环的学习记录

--- study/day03/xun_huan/main.go
+++ study/day03/xun_huan/main.go
@@ -16,6 +16,106 @@ func testFor1() {
 	}
 }
 
+func testFor2() {
+	// 对每次循环前开始前计算的表达式，如果bool值为true，则继续往下执行。否则结束循环。
+	// 条件表达式可以忽略，但是循环默认成了无限循环
+
+	// 打印 5-10 的数字
+	num := 10
+	for ; num >= 5; num-- {
+		fmt.Println(num)
+	}
+
+	// 无限循环
+	for ; ; num++ {
+		fmt.Println(num)
+		if num == 10 {
+			break
+		}
+	}
+
+	fmt.Printf("-----------------%d---------------------\n", num)
+
+	// 更美观的写法：省略初始语句、条件表达式、结束语句
+	for {
+		fmt.Println(num)
+		if num == 100 {
+			break
+		}
+
+		num++
+	}
+
+	num = 0
+	// if 和 for 进一步整合
+	for num != 10 {
+		fmt.Println(num)
+		num++
+	}
+
+}
+
+func testFor3() {
+	// 结束语句：在每次循环结束之前执行的语句。但是当循环碰break、goto、return、panic等语句强制退出时，结束语句不会执行。
+}
+
+func jiujiu() {
+	for i := 1; i < 10; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d * %d = %d\t", i, j, i*j)
+		}
+		fmt.Println()
+	}
+}
+
+func forArray() {
+	array1 := [...]string{"Python", "Go", "Java", "Linux"}
+
+	for k, v := range array1 {
+		fmt.Println(k, v)
+	}
+
+	slice1 := []string{"Python", "Go", "Java", "Linux"}
+	slice1 = append(slice1, "C++")
+	for k, v := range slice1 {
+		fmt.Println(k, v)
+	}
+}
+
+func forString() {
+	str1 := "Life Is Short You Need Python --- 江子牙"
+
+	// v 变量其实是rune类型， 也就是int32 可以用%c占位符来显示
+	for k, v := range str1 {
+		fmt.Printf("index: %d  val: %c\n", k, v)
+	}
+
+}
+
+func forMap() {
+	mp := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+
+	for k, v := range mp {
+		fmt.Println(k, v)
+	}
+}
+
+func testKV() {
+	// 遍历数组、切片
+	// forArray()
+
+	// 遍历字符串
+	// forString()
+
+	// 遍历map
+	forMap()
+
+}
+
 func main() {
 	fmt.Println("循环")
 
@@ -28,7 +128,18 @@ func main() {
 	*/
 
 	// for 循环的初始语句---开始循环时执行的语句
-	testFor1()
+	// testFor1()
+
+	// for 循环的条件表达式---控制是否循环的开关
 	// testFor2()
+
+	// for 循环的结束语句---每次循环结束时的执行语句
 	// testFor3()
+
+	// 九九乘法表
+	// jiujiu()
+
+	// 键值循环
+	testKV()
+
 }
