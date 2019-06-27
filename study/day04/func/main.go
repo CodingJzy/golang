25612e9ed4b2d2ba2e5d1b4f0e184f608c13e96d
CodingJzy <jw19961019@gmail.com>
2019-06-27 11:06:59 +0800

    添加函数学习记录，学习进度为可变参数。

--- study/day04/func/main.go
+++ study/day04/func/main.go
@@ -150,6 +150,111 @@ func testAnonymous() {
 	// 运行命令：go run main.go   --skill=run
 }
 
+// 定义一个函数，返回值为一个匿名函数，匿名函数与原函数形成闭包，把name 、hp 返回
+func palyerGen(name string) func() (string, int) {
+
+	hp := 150
+
+	return func() (string, int) {
+		return name, hp
+	}
+}
+
+func testClosure() {
+	// 闭包 = 函数 + 引用环境
+	// 函数是编译期静态的概念，而闭包是运行期动态的概念
+
+	// 闭包内部修改引用的变量
+	str1 := "hello world"
+	fmt.Printf("修改之前---%s\n", str1)
+	// 创建一个匿名函数,并直接调用
+	func() {
+		str1 = "hello go"
+	}()
+	fmt.Printf("修改后---%s\n", str1)
+	// 匿名函数中并没有定义str1，也不是通过参数传递的方式。就算通过传递参俗话的方式。由于字符串不可变。不会对原有字符串发生改变。
+	// str1 的定义在匿名函数之前。此时，str1就被引用到匿名函数中形成了闭包
+
+	// 闭包实现生成器
+	// 调用函数
+	gen := palyerGen("天使彦")
+	// 返回天使彦的名字和血量
+	name, hp := gen()
+	// 输出
+	fmt.Println(name, hp)
+	// 可以看出闭包还具有一定的封装性，血量是无法从外部修改的，与面向对象的封装类似。限制外部内内部的访问权限
+}
+
+func studentInfo1(name string, age int, args ...string) {
+	fmt.Println("固定参数", name, age)
+	fmt.Println("不固定参数", args)
+
+	// 通过遍历获取每一个参数
+	for index, arg := range args {
+		fmt.Printf("索引：%d  参数值：%s\n", index, arg)
+	}
+}
+
+func studentInfo2(name string, args ...interface{}) {
+	fmt.Println("固定参数", name)
+	fmt.Println("不固定参数", args)
+
+	// 通过遍历获取每一个参数的类型
+	for k, v := range args {
+
+		// 对参数的值进行格式化
+		value := fmt.Sprintf("%v", v)
+
+		fmt.Printf("索引：%d  \t参数的类型：%T\t参数值：%s\n", k, v, value)
+	}
+
+}
+
+func printInfo(slices ...interface{}) {
+
+	for index, arg := range slices {
+		value := fmt.Sprintf("%v", arg)
+		fmt.Printf("索引：%d  参数值：%s\n", index, value)
+	}
+}
+
+func studentInfo3(slices ...interface{}) {
+	// 将slices 可变参数切片完整的传递给下一个打印函数
+	printInfo(slices...)
+}
+
+func testArgs() {
+	// 有时候我们不知道我们传的参数有多少个，可以通过不固定参数来解决。类似python中的 *args **kwargs
+
+	/*
+		格式：
+			func 函数名 (固定参数列表, v ... T) (返回参数列表) {
+				函数体
+			}
+		v:
+			为可变参数变量，类型为[]T ，也就是拥有多个元素的类型切片 ，v 和 t 之间用 ...
+		T:
+			为可变参数类型。当类型为interface{}的时候，传入的值可以是任意值
+	*/
+
+	// 可变参数为多个字符串元素的切片类型。并循环遍历每个参数
+	// studentInfo1("江子牙", 21, "江西省", "萍乡市", "莲花县", "坊楼镇", "小江村")
+
+	// 可变参数为interface{}类型。并循环遍历每个参数的类型
+	// studentInfo2("江子牙", 22, 99.9, "sex", true, map[string]string{
+	// 	"className": "高三(1)班",
+	// 	"score":     "99",
+	// })
+
+	// 可变参数本身就是一个切片类型，当我们想传多个参数列表的切片的时候，可以这样
+
+	studentInfo3("江子牙", 22, 99.9, "sex", true, map[string]string{
+		"className": "高三(1)班",
+		"score":     "99",
+	})
+
+}
+
 // 声明一个命令行全局变量
 var skillParam = flag.String("skill", "", "skill to perform")
 
@@ -163,13 +268,15 @@ func main() {
 	// testSave()
 
 	// 匿名函数
-	testAnonymous()
+	// testAnonymous()
 
 	// 函数类型实现接口---把函数作为接口来调用
 
-	// 闭包---引用了外部变量的匿名函数
+	// 闭包(Closure)---引用了外部变量的匿名函数
+	// testClosure()
 
 	// 可变参数---参数数量不固定的函数形式
+	// testArgs()
 
 	// 延迟执行语句(defer)
 
