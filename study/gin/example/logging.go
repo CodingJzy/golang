9ac7b628e7bd8a5f37d445842c6ca8c594142ef6
CodingJzy <jw19961019@gmail.com>
2019-07-03 11:17:19 +0800

    gin 文件形式记录日志

---
+++ study/gin/example/logging.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"os"
+)
+
+func main() {
+	// 禁言控制台颜色，将日志写入文件时不需要控制台颜色
+	gin.DisableConsoleColor()
+
+	// 记录到文件
+	f, _ := os.Create("log/gin.log1")
+	fmt.Println(f)
+	gin.DefaultWriter = io.MultiWriter(f)
+
+	// 同时将日志写入文件和控制台
+	//gin.DefaultWriter = io.MultiWriter(f,os.Stdin)
+	r := gin.Default()
+
+	r.GET("/log", func(c *gin.Context) {
+		c.String(200, "log")
+	})
+	r.Run()
+}
