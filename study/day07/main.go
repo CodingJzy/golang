5bc831a2649184f9e46c11a37c2b6a2e04788089
CodingJzy <jw19961019@gmail.com>
2019-07-04 10:50:06 +0800

    go 包和模块的记录

---
+++ study/day07/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"golang/study/day07/mylib"
+)
+
+// 1、全局变量
+var a1 = 9
+
+// 2、init()
+func init() {
+	fmt.Println("开始执行init()")
+	a2 := 10
+	fmt.Printf("输出局部变量%d\n", a2)
+	fmt.Printf("在init()里输出全局变量%d\n", a1)
+	fmt.Println("准备执行main()")
+}
+
+// 3、main()
+func main() {
+	fmt.Println("开始执行main")
+	fmt.Printf("在main()里输出全局变量%d", a1)
+
+	// 调用mylib包下的Add()加法函数
+
+}
