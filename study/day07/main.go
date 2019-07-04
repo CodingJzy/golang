7d49963d7f353af4d0adc9ec19685b37d1f28670
CodingJzy <jw19961019@gmail.com>
2019-07-04 11:03:21 +0800

    go 调用mylib包下的Add()加法函数

--- study/day07/main.go
+++ study/day07/main.go
@@ -20,8 +20,8 @@ func init() {
 // 3、main()
 func main() {
 	fmt.Println("开始执行main")
-	fmt.Printf("在main()里输出全局变量%d", a1)
+	fmt.Printf("在main()里输出全局变量%d\n", a1)
 
 	// 调用mylib包下的Add()加法函数
-
+	print(mylib.Add(10, 10))
 }
